b5736a001a77a6677d8cd4d9fe9f28446f78fbca
Chris Banes <chrisbanes@google.com>
2017-12-18 15:14:26 +0000

    Add extensions for View

---
+++ src/main/java/androidx/view/View.kt
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:Suppress("NOTHING_TO_INLINE") // Aliases to other public API.
+
+package androidx.view
+
+import android.support.annotation.RequiresApi
+import android.view.View
+import android.view.ViewTreeObserver
+
+/**
+ * Performs the given action when this view is next laid out.
+ *
+ * @see doWhenLaidOut
+ */
+@RequiresApi(14)
+inline fun View.doOnLayout(crossinline action: (View) -> Unit) {
+    addOnLayoutChangeListener(object : View.OnLayoutChangeListener {
+        override fun onLayoutChange(view: View, left: Int, top: Int, right: Int, bottom: Int,
+                                    oldLeft: Int, oldTop: Int, oldRight: Int, oldBottom: Int) {
+            action(view)
+            view.removeOnLayoutChangeListener(this)
+        }
+    })
+}
+
+/**
+ * Performs the given action when this view is laid out. If the view has not laid out yet,
+ * the action will be performed after the view is laid out.
+ *
+ * @see doOnLayout
+ */
+@RequiresApi(19)
+fun View.doWhenLaidOut(action: (View) -> Unit) {
+    if (isLaidOut) {
+        action(this)
+    } else {
+        doOnLayout {
+            action(it)
+        }
+    }
+}
+
+/**
+ * Performs the given action when the view tree is about to be drawn.
+ */
+fun View.doOnPreDraw(action: (View) -> Unit) {
+    val vto = viewTreeObserver
+    vto.addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener {
+        override fun onPreDraw(): Boolean {
+            action(this@doOnPreDraw)
+            when {
+                vto.isAlive -> vto.removeOnPreDrawListener(this)
+                else -> viewTreeObserver.removeOnPreDrawListener(this)
+            }
+            return true
+        }
+    })
+}
+
+/**
+ * Updates this view's relative padding. This version of the method allows using named parameters
+ * to set just 1 or more axis.
+ *
+ * @see View.setPaddingRelative
+ */
+@RequiresApi(17)
+fun View.updatePaddingRelative(start: Int = paddingStart,
+                               top: Int = paddingTop,
+                               end: Int = paddingEnd,
+                               bottom: Int = paddingBottom) {
+    setPaddingRelative(start, top, end, bottom)
+}
+
+/**
+ * Updates this view's padding. This version of the method allows using named parameters
+ * to set just 1 or more axis.
+ *
+ * @see View.setPadding
+ */
+fun View.updatePadding(left: Int = paddingLeft,
+                       top: Int = paddingTop,
+                       right: Int = paddingRight,
+                       bottom: Int = paddingBottom) {
+    setPadding(left, top, right, bottom)
+}
---
+++ src/test/java/androidx/view/ViewTest.kt
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.view
+
+import android.view.View
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.Config.NEWEST_SDK
+
+@RunWith(RobolectricTestRunner::class)
+@Config(manifest = Config.NONE)
+class ViewTest {
+    private val context = RuntimeEnvironment.application
+    private val view = View(context)
+
+    @Test
+    fun doOnLayout() {
+        var called = false
+        view.doOnLayout {
+            called = true
+        }
+        view.layout(0, 0, 10, 10)
+        assertTrue(called)
+    }
+
+    @Test
+    @Config(sdk = [NEWEST_SDK])
+    fun doWhenLaidOutBeforeLayout() {
+        var called = false
+        view.doWhenLaidOut {
+            called = true
+        }
+        view.layout(0, 0, 10, 10)
+        assertTrue(called)
+    }
+
+    @Test
+    @Config(sdk = [NEWEST_SDK])
+    fun doWhenLaidOutAfterLayout() {
+        view.layout(0, 0, 10, 10)
+
+        var called = false
+        view.doWhenLaidOut {
+            called = true
+        }
+        assertTrue(called)
+    }
+
+    @Test
+    fun doOnPreDraw() {
+        var called = false
+        view.doOnPreDraw {
+            called = true
+        }
+        view.viewTreeObserver.dispatchOnPreDraw()
+        assertTrue(called)
+    }
+
+    @Test
+    fun updatePadding() {
+        view.updatePadding(top = 10, right = 20)
+        assertEquals(0, view.paddingLeft)
+        assertEquals(10, view.paddingTop)
+        assertEquals(20, view.paddingRight)
+        assertEquals(0, view.paddingBottom)
+    }
+
+    @Test
+    @Config(sdk = [NEWEST_SDK])
+    fun updatePaddingRelative() {
+        view.updatePaddingRelative(start = 10, end = 20)
+        assertEquals(10, view.paddingStart)
+        assertEquals(0, view.paddingTop)
+        assertEquals(20, view.paddingEnd)
+        assertEquals(0, view.paddingBottom)
+    }
+}
