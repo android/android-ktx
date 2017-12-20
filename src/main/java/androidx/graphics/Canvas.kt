f8db75f921bf8ff3977a31fd7a4d85afc761eede
Romain Guy <romainguy@curious-creature.com>
2017-12-20 10:00:59 -0500

    Add Canvas.withSave extension
    Automatically wraps a block with a save/restoreToCount pair:
    
    Canvas.withSave {
        drawLine(…)
        scale(…)
        // etc.
    }

--- src/main/java/androidx/graphics/Canvas.kt
+++ src/main/java/androidx/graphics/Canvas.kt
@@ -19,6 +19,19 @@ package androidx.graphics
 import android.graphics.Canvas
 
 /**
+ * Wrap the specified `block` in calls to [Canvas.save]
+ * and [Canvas.restoreToCount].
+ */
+inline fun Canvas.withSave(block: Canvas.() -> Unit) {
+    val checkpoint = save()
+    try {
+        block()
+    } finally {
+        restoreToCount(checkpoint)
+    }
+}
+
+/**
  * Wrap the specified `block` in calls to [Canvas.save]/[Canvas.translate]
  * and [Canvas.restoreToCount].
  */
