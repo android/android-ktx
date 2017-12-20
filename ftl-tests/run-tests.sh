dbbe2b72ac93358bf702c5db635d82cf9454bc47
Chris Banes <chrisbanes@google.com>
2017-12-20 16:38:07 +0000

    Disable FTL features we don't need

--- ftl-tests/run-tests.sh
+++ ftl-tests/run-tests.sh
@@ -6,5 +6,7 @@ gcloud firebase test android run \
     --app ftl-tests/dummy.apk \
     --test build/outputs/apk/androidTest/debug/kotlin-extensions-debug-androidTest.apk \
     --device model=Nexus6P,version=27,locale=en_US,orientation=portrait \
-    --timeout 20m \
-    --results-bucket cloud-test-android-devrel-ci
+    --timeout 30m \
+    --results-bucket cloud-test-android-devrel-ci \
+    --no-record-video \
+    --no-performance-metrics
