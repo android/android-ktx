9390a7d9d7384e3171d80a761d9fc9b81076f92c
Romain Guy <romainguy@google.com>
2018-01-11 11:15:50 -0800

    Add TypedArray related extensions to Context

--- src/main/java/androidx/content/Context.kt
+++ src/main/java/androidx/content/Context.kt
@@ -1,7 +1,9 @@
 package androidx.content
 
 import android.content.Context
+import android.content.res.TypedArray
 import android.support.annotation.RequiresApi
+import android.util.AttributeSet
 
 /**
  * Return the handle to a system-level service by class.
@@ -15,3 +17,103 @@ import android.support.annotation.RequiresApi
 @RequiresApi(23)
 @Suppress("HasPlatformType") // Intentionally propagating platform type with unknown nullability.
 inline fun <reified T> Context.systemService() = getSystemService(T::class.java)
+
+/**
+ * Executes [block] on a [TypedArray] receiver. The [TypedArray] holds the attribute
+ * values in [set] that are listed in [attrs]. In addition, if the given [AttributeSet]
+ * specifies a style class (through the `style` attribute), that style will be applied
+ * on top of the base attributes it defines.
+ *
+ * @param set The base set of attribute values.
+ * @param attrs	The desired attributes to be retrieved. These attribute IDs must be
+ *              sorted in ascending order.
+ *
+ * @see Context.obtainStyledAttributes
+ * @see android.content.res.Resources.Theme.obtainStyledAttributes
+ */
+inline fun Context.withStyledAttributes(
+        set: AttributeSet?,
+        attrs: IntArray,
+        block: TypedArray.() -> Unit) {
+    val typedArray = obtainStyledAttributes(set, attrs)
+    try {
+        typedArray.block()
+    } finally {
+        typedArray.recycle()
+    }
+}
+
+/**
+ * Executes [block] on a [TypedArray] receiver. The [TypedArray] holds the attribute
+ * values in [set] that are listed in [attrs]. In addition, if the given [AttributeSet]
+ * specifies a style class (through the `style` attribute), that style will be applied
+ * on top of the base attributes it defines.
+ *
+ * @param set The base set of attribute values.
+ * @param attrs	The desired attributes to be retrieved. These attribute IDs must be
+ *              sorted in ascending order.
+ * @param defStyleAttr An attribute in the current theme that contains a reference to
+ *                     a style resource that supplies defaults values for the [TypedArray].
+ *                     Can be 0 to not look for defaults.
+ * @param defStyleRes A resource identifier of a style resource that supplies default values
+ *                    for the [TypedArray], used only if [defStyleAttr] is 0 or can not be found
+ *                     in the theme. Can be 0 to not look for defaults.
+ *
+ * @see Context.obtainStyledAttributes
+ * @see android.content.res.Resources.Theme.obtainStyledAttributes
+ */
+inline fun Context.withStyledAttributes(
+        set: AttributeSet?,
+        attrs: IntArray,
+        defStyleAttr: Int,
+        defStyleRes: Int,
+        block: TypedArray.() -> Unit) {
+    val typedArray = obtainStyledAttributes(set, attrs)
+    try {
+        typedArray.block()
+    } finally {
+        typedArray.recycle()
+    }
+}
+
+/**
+ * Executes [block] on a [TypedArray] receiver. The [TypedArray] holds the the values
+ * defined by this context's [theme][android.content.res.Resources.Theme] which are
+ * listed in [attrs].
+ *
+ * @param attrs	The desired attributes. These attribute IDs must be sorted in ascending order.
+ *
+ * @see Context.obtainStyledAttributes
+ * @see android.content.res.Resources.Theme.obtainStyledAttributes
+ */
+inline fun Context.withStyledAttributes(
+        attrs: IntArray,
+        block: TypedArray.() -> Unit) {
+    val typedArray = obtainStyledAttributes(attrs)
+    try {
+        typedArray.block()
+    } finally {
+        typedArray.recycle()
+    }
+}
+
+/**
+ * Executes [block] on a [TypedArray] receiver. The [TypedArray] holds the the values
+ * defined by the style resource [resourceId] which are listed in [attrs].
+ *
+ * @param attrs	The desired attributes. These attribute IDs must be sorted in ascending order.
+ *
+ * @see Context.obtainStyledAttributes
+ * @see android.content.res.Resources.Theme.obtainStyledAttributes
+ */
+inline fun Context.withStyledAttributes(
+        resourceId: Int,
+        attrs: IntArray,
+        block: TypedArray.() -> Unit) {
+    val typedArray = obtainStyledAttributes(resourceId, attrs)
+    try {
+        typedArray.block()
+    } finally {
+        typedArray.recycle()
+    }
+}
