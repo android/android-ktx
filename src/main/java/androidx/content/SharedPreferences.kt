036f2f20766070082def78729c6d1163bbb858e4
UmangChamaria <umangchamaria@gmail.com>
2018-02-06 20:21:23 +0530

    Update SharedPreferences editor to allow commit.

--- src/main/java/androidx/content/SharedPreferences.kt
+++ src/main/java/androidx/content/SharedPreferences.kt
@@ -16,20 +16,36 @@
 
 package androidx.content
 
+import android.annotation.SuppressLint
 import android.content.SharedPreferences
 
 /**
- * Allows editing of this preference instance with a call to
- * [SharedPreferences.Editor.apply] to persist the changes.
- *
+ * Allows editing of this preference instance with a call to [apply][SharedPreferences.Editor.apply]
+ * or [commit][SharedPreferences.Editor.commit] to persist the changes.
+ * Default behaviour is [apply][SharedPreferences.Editor.apply].
  * ```
  * prefs.edit {
  *     putString("key", value)
  * }
  * ```
+ * To [commit][SharedPreferences.Editor.commit] changes:
+ * ```
+ * prefs.edit(commit = true) {
+ *     putString("key", value)
+ * }
+ * ```
  */
-inline fun SharedPreferences.edit(action: SharedPreferences.Editor.() -> Unit) {
+@SuppressLint("ApplySharedPref")
+@JvmOverloads // For binary compatibility with 0.1. TODO remove for 1.0.
+inline fun SharedPreferences.edit(
+    commit: Boolean = false,
+    action: SharedPreferences.Editor.() -> Unit
+) {
     val editor = edit()
     action(editor)
-    editor.apply()
+    if (commit) {
+        editor.commit()
+    } else {
+        editor.apply()
+    }
 }
