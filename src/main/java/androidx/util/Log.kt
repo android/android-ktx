23d12372ceab278fb93b7bb1bd08cd82de9bbc3c
Sergey Mischenko <vitaminpsg@gmail.com>
2018-02-08 21:57:42 +0300

    Add 'log' extension Remove useless function from log

--- src/main/java/androidx/util/Log.kt
+++ src/main/java/androidx/util/Log.kt
@@ -20,87 +20,39 @@ package androidx.util
 
 import android.util.Log
 
-fun <T : Any> T.v(msg: () -> String) = v(msg())
+inline fun <T : Any> T.v(msg: () -> String) = v(msg())
 
-fun <T : Any> T.d(msg: () -> String) = d(msg())
+inline fun <T : Any> T.d(msg: () -> String) = d(msg())
 
-fun <T : Any> T.i(msg: () -> String) = i(msg())
+inline fun <T : Any> T.i(msg: () -> String) = i(msg())
 
-fun <T : Any> T.w(msg: () -> String) = w(msg())
+inline fun <T : Any> T.w(msg: () -> String) = w(msg())
 
-fun <T : Any> T.e(msg: () -> String) = e(msg())
+inline fun <T : Any> T.e(msg: () -> String) = e(msg())
 
-fun <T : Any> T.wtf(msg: () -> String) = wtf(msg())
+inline fun <T : Any> T.wtf(msg: () -> String) = wtf(msg())
 
-fun <T : Any> T.v(msg: String, vararg args: Any) {
-    formatMessage(msg, args) { v(it) }
-}
-
-fun <T : Any> T.d(msg: String, vararg args: Any) {
-    formatMessage(msg, args) { d(it) }
-}
-
-fun <T : Any> T.i(msg: String, vararg args: Any) {
-    formatMessage(msg, args) { i(it) }
-}
-
-fun <T : Any> T.w(msg: String, vararg args: Any) {
-    formatMessage(msg, args) { w(it) }
-}
-
-fun <T : Any> T.e(msg: String, vararg args: Any) {
-    formatMessage(msg, args) { e(it) }
-}
-
-fun <T : Any> T.wtf(msg: String, vararg args: Any) {
-    formatMessage(msg, args) { wtf(it) }
-}
-
-fun <T : Any> T.v(msg: String) {
+inline fun <T : Any> T.v(msg: String) {
     if (Log.isLoggable(javaClass.simpleName, Log.VERBOSE)) v(javaClass.simpleName, msg)
 }
 
-fun <T : Any> T.d(msg: String) {
+inline fun <T : Any> T.d(msg: String) {
     if (Log.isLoggable(javaClass.simpleName, Log.DEBUG)) d(javaClass.simpleName, msg)
 }
 
-fun <T : Any> T.i(msg: String) {
+inline fun <T : Any> T.i(msg: String) {
     if (Log.isLoggable(javaClass.simpleName, Log.INFO)) i(javaClass.simpleName, msg)
 }
 
-fun <T : Any> T.w(msg: String) {
+inline fun <T : Any> T.w(msg: String) {
     if (Log.isLoggable(javaClass.simpleName, Log.WARN)) w(javaClass.simpleName, msg)
 }
 
-fun <T : Any> T.e(msg: String) {
+inline fun <T : Any> T.e(msg: String) {
     if (Log.isLoggable(javaClass.simpleName, Log.ERROR)) e(javaClass.simpleName, msg)
 }
 
-fun <T : Any> T.wtf(msg: String) = wtf(javaClass.simpleName, msg)
-
-fun v(tag: String, msg: String, vararg args: Any) {
-    formatMessage(msg, args) { v(tag, it) }
-}
-
-fun d(tag: String, msg: String, vararg args: Any) {
-    formatMessage(msg, args) { d(tag, it) }
-}
-
-fun i(tag: String, msg: String, vararg args: Any) {
-    formatMessage(msg, args) { i(tag, it) }
-}
-
-fun w(tag: String, msg: String, vararg args: Any) {
-    formatMessage(msg, args) { w(tag, it) }
-}
-
-fun e(tag: String, msg: String, vararg args: Any) {
-    formatMessage(msg, args) { e(tag, it) }
-}
-
-fun wtf(tag: String, msg: String, vararg args: Any) {
-    formatMessage(msg, args) { wtf(tag, it) }
-}
+inline fun <T : Any> T.wtf(msg: String) = wtf(javaClass.simpleName, msg)
 
 inline fun v(tag: String, msg: String) = Log.v(tag, msg)
 
@@ -113,13 +65,3 @@ inline fun w(tag: String, msg: String) = Log.w(tag, msg)
 inline fun e(tag: String, msg: String) = Log.e(tag, msg)
 
 inline fun wtf(tag: String, msg: String) = Log.wtf(tag, msg)
-
-fun formatMessage(
-    msg: String,
-    vararg args: Any,
-    block: (String) -> Unit
-) {
-    if (args.isNotEmpty()) {
-        block(String.format(msg, args))
-    }
-}
