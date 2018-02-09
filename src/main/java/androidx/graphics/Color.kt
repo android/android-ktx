cecddc1bd51939b7faa3e779daf33f284a64ff10
Roman Zavarnitsyn <rom4ek93@gmail.com>
2018-02-09 22:36:26 +0100

    Improve docs

--- src/main/java/androidx/graphics/Color.kt
+++ src/main/java/androidx/graphics/Color.kt
@@ -378,5 +378,18 @@ inline val @receiver:ColorLong Long.colorSpace: ColorSpace get() = Color.colorSp
 
 /**
  * Return a corresponding [Int] color of this [String].
+ *
+ * Supported formats are:
+ * ```
+ * #RRGGBB
+ * #AARRGGBB
+ * ```
+ *
+ * The following names are also accepted: "red", "blue", "green", "black", "white",
+ * "gray", "cyan", "magenta", "yellow", "lightgray", "darkgray",
+ * "grey", "lightgrey", "darkgrey", "aqua", "fuchsia", "lime",
+ * "maroon", "navy", "olive", "purple", "silver", "teal".
+ *
+ * @throws IllegalArgumentException if this [String] cannot be parsed.
  */
 inline fun String.toColorInt(): Int = Color.parseColor(this)
