6fd8e159dcffc5e5823e60b5f921a7ff7d4cc2c9
a.fialko <a.fialko@evotor.ru>
2018-02-09 15:43:55 +0300

    add documentation

--- src/main/java/androidx/widget/RemoteViews.kt
+++ src/main/java/androidx/widget/RemoteViews.kt
@@ -24,34 +24,46 @@ import android.support.annotation.ColorInt
 import android.support.annotation.DimenRes
 import android.support.annotation.DrawableRes
 import android.support.annotation.IdRes
+import android.support.annotation.IntRange
 import android.widget.RemoteViews
 
+/** Set `textSize` to 'TextView' in 'RemoteView'. */
 inline fun RemoteViews.setTextViewTextSize(@IdRes viewId: Int, @DimenRes size: Float) =
     setFloat(viewId, "setTextSize", size)
 
+/** Set `minLines` to 'TextView' in 'RemoteView'. */
 inline fun RemoteViews.setTextViewMinLines(@IdRes viewId: Int, @DimenRes lines: Int) =
     setInt(viewId, "setMinLines", lines)
 
+/** Set `maxLines` to 'TextView' in 'RemoteView'. */
 inline fun RemoteViews.setTextViewMaxLines(@IdRes viewId: Int, @DimenRes lines: Int) =
     setInt(viewId, "setMaxLines", lines)
 
+/** Set `paintFlags` to 'TextView' in 'RemoteView'. */
 inline fun RemoteViews.setTextViewPaintFlags(@IdRes viewId: Int, flags: Int) =
     setInt(viewId, "setPaintFlags", flags)
 
-inline fun RemoteViews.setImageViewAlpha(@IdRes viewId: Int, @DimenRes alpha: Int) =
+/** Set `alpha` to 'ImageView' in 'RemoteView'. */
+inline fun RemoteViews.setImageViewAlpha(
+    @IdRes viewId: Int, @IntRange(from = 0, to = 255) alpha: Int) =
     setInt(viewId, "setImageAlpha", alpha)
 
+/** Set `maxWidth` to 'ImageView' in 'RemoteView'. */
 inline fun RemoteViews.setImageViewMaxWidth(@IdRes viewId: Int, @DimenRes width: Int) =
     setInt(viewId, "setMaxWidth", width)
 
+/** Set `maxHeight` to 'ImageView' in 'RemoteView'. */
 inline fun RemoteViews.setImageViewMaxHeight(@IdRes viewId: Int, @DimenRes height: Int) =
     setInt(viewId, "setMaxHeight", height)
 
+/** Set `enabled` to 'View' in 'RemoteView'. */
 inline fun RemoteViews.setEnabled(@IdRes viewId: Int, enabled: Boolean) =
     setBoolean(viewId, "setEnabled", enabled)
 
+/** Set `background` to 'View' in 'RemoteView' with color. */
 inline fun RemoteViews.setBackgroundColor(@IdRes viewId: Int, @ColorInt color: Int) =
     setInt(viewId, "setBackgroundColor", color)
 
+/** Set `background` to 'View' in 'RemoteView' with drawable. */
 inline fun RemoteViews.setBackgroundResource(@IdRes viewId: Int, @DrawableRes drawable: Int) =
     setInt(viewId, "setBackgroundResource", drawable)
