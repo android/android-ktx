323b6296dcbfadf345e16b85a3ea3a0c56cf3796
Sergey Vasilinets <sergeyv@google.com>
2018-02-20 09:04:52 -0800

    Add missing copyright and rename parameters

--- src/main/java/androidx/util/LruCache.kt
+++ src/main/java/androidx/util/LruCache.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package androidx.util
 
 import android.util.LruCache
@@ -18,12 +34,12 @@ private class DelegateLruCache<K, V>(
 /**
  * Creates [android.util.LruCache] with the given parameters.
  *
- * @param maxSize for caches that do not specify [sizeFunc], this is
+ * @param maxSize for caches that do not specify [onSizeOf], this is
  * the maximum number of entries in the cache. For all other caches,
  * this is the maximum sum of the sizes of the entries in this cache.
- * @param sizeFunc function that returns the size of the entry for key and value in
+ * @param onSizeOf function that returns the size of the entry for key and value in
  * user-defined units. The default implementation returns 1.
- * @param factory a factory called after a cache miss to compute a value for the corresponding key.
+ * @param onCreate a onCreate called after a cache miss to compute a value for the corresponding key.
  * Returns the computed value or null if no value can be computed. The default implementation
  * returns null.
  * @param onEntryRemoved a function called for entries that have been evicted or removed.
@@ -33,9 +49,9 @@ private class DelegateLruCache<K, V>(
  * @see LruCache.entryRemoved
  */
 fun <K, V> lruCache(maxSize: Int,
-                    sizeFunc: (K, V) -> Int = { _, _ -> 1 },
-                    factory: (K) -> V? = { _ -> null },
+                    onSizeOf: (K, V) -> Int = { _, _ -> 1 },
+                    onCreate: (K) -> V? = { _ -> null },
                     onEntryRemoved: ((Boolean, K, V, V?) -> Unit) = { _, _, _, _ -> }):
         LruCache<K, V> {
-    return DelegateLruCache(maxSize, sizeFunc, factory, onEntryRemoved)
+    return DelegateLruCache(maxSize, onSizeOf, onCreate, onEntryRemoved)
 }
