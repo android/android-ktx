1c4a82a54ebe33c260988ee1d03bb1e03e5c7f22
Joshua Kovach <kovach.jc@gmail.com>
2018-03-07 13:59:00 -0500

    Expand tests for tighter feature coverage

--- src/androidTest/java/androidx/text/SpannableStringTest.kt
+++ src/androidTest/java/androidx/text/SpannableStringTest.kt
@@ -89,11 +89,10 @@ class SpannableStringTest {
         assertEquals(12, s.getSpanEnd(underline))
     }
 
-    @Test fun overlay() {
+    @Test fun overlay_MultipleSpans() {
         val s = "Hello, World".toSpannable()
         assertEquals(5, s.overlay("Hello", StyleSpan(BOLD)))
         assertEquals(12, s.overlay("World", UnderlineSpan(), true))
-        assertEquals(-1, s.overlay("world", ForegroundColorSpan(Color.BLUE)))
 
         val spans = s.getSpans<Any>()
 
@@ -104,22 +103,106 @@ class SpannableStringTest {
         val underline = spans.filterIsInstance<UnderlineSpan>().single()
         assertEquals(7, s.getSpanStart(underline))
         assertEquals(12, s.getSpanEnd(underline))
-
-        val blue = spans.filterIsInstance<ForegroundColorSpan>()
-        assertEquals(emptyList<Any>(), blue)
     }
 
-    @Test fun overlayAll() {
+    @Test fun overlay_CaseSensitiveNoMatch() {
+        val s = "Hello, World".toSpannable()
+        assertEquals(-1, s.overlay("world", ForegroundColorSpan(Color.BLUE)))
+
+        val spans = s.getSpans<Any>()
+        assertEquals(0, spans.size)
+    }
+
+    @Test fun overlay_CaseSensitiveMatch() {
+        val s = "Hello, World".toSpannable()
+        assertEquals(12, s.overlay("World", ForegroundColorSpan(Color.BLUE)))
+
+        val spans = s.getSpans<Any>()
+
+        val blue = spans.filterIsInstance<ForegroundColorSpan>().single()
+        assertEquals(7, s.getSpanStart(blue))
+        assertEquals(12, s.getSpanEnd(blue))
+    }
+
+    @Test fun overlay_CaseInsensitiveMatch() {
+        val s = "Hello, World".toSpannable()
+        assertEquals(12, s.overlay("world", ForegroundColorSpan(Color.BLUE), true))
+
+        val spans = s.getSpans<Any>()
+
+        val blue = spans.filterIsInstance<ForegroundColorSpan>().single()
+        assertEquals(7, s.getSpanStart(blue))
+        assertEquals(12, s.getSpanEnd(blue))
+    }
+
+    @Test fun overlay_StartAfterFirst() {
+        val s = "Hello, World, World".toSpannable()
+        assertEquals(19, s.overlay("World", ForegroundColorSpan(Color.BLUE), false, 8))
+
+        val spans = s.getSpans<Any>()
+
+        val blue = spans.filterIsInstance<ForegroundColorSpan>().single()
+        assertEquals(14, s.getSpanStart(blue))
+        assertEquals(19, s.getSpanEnd(blue))
+    }
+
+    @Test fun overlay_MultipleOccurrencesAppliesOnce() {
+        val s = "Hello, World, World".toSpannable()
+        assertEquals(12, s.overlay("World", ForegroundColorSpan(Color.BLUE)))
+
+        val spans = s.getSpans<Any>()
+
+        assertEquals(1, spans.size)
+
+        val blue = spans.filterIsInstance<ForegroundColorSpan>().single()
+        assertEquals(7, s.getSpanStart(blue))
+        assertEquals(12, s.getSpanEnd(blue))
+    }
+
+    @Test fun overlayAll_MultipleSpans() {
         val s = "Hello, World, I'm world".toSpannable()
         s.overlayAll("Hello") { StyleSpan(BOLD) }
         s.overlayAll("World", true) { UnderlineSpan() }
         s.overlayAll("world") { ForegroundColorSpan(Color.BLUE) }
 
         val spans = s.getSpans<Any>()
+        assertEquals(4, spans.size)
 
-        val bold = spans.filterIsInstance<StyleSpan>().single()
-        assertEquals(0, s.getSpanStart(bold))
-        assertEquals(5, s.getSpanEnd(bold))
+        val bolds = spans.filterIsInstance<StyleSpan>()
+        assertEquals(1, bolds.size)
+
+        val underlines = spans.filterIsInstance<UnderlineSpan>()
+        assertEquals(2, underlines.size)
+
+        val blues = spans.filterIsInstance<ForegroundColorSpan>()
+        assertEquals(1, blues.size)
+    }
+
+    @Test fun overlayAll_CaseSensitiveNoMatch() {
+        val s = "Hello, World, I'm World".toSpannable()
+        s.overlayAll("world") { ForegroundColorSpan(Color.BLUE) }
+
+        val spans = s.getSpans<Any>()
+        assertEquals(0, spans.size)
+    }
+
+    @Test fun overlayAll_CaseSensitiveMatch() {
+        val s = "Hello, World, I'm world".toSpannable()
+        s.overlayAll("world") { ForegroundColorSpan(Color.BLUE) }
+
+        val spans = s.getSpans<Any>()
+        assertEquals(1, spans.size)
+
+        val blue = spans.filterIsInstance<ForegroundColorSpan>().single()
+        assertEquals(18, s.getSpanStart(blue))
+        assertEquals(23, s.getSpanEnd(blue))
+    }
+
+    @Test fun overlayAll_CaseInsensitiveMultipleMatch() {
+        val s = "Hello, World, I'm world".toSpannable()
+        s.overlayAll("World", true) { UnderlineSpan() }
+
+        val spans = s.getSpans<Any>()
 
         val underlines = spans.filterIsInstance<UnderlineSpan>().sortedBy { s.getSpanStart(it) }
         assertEquals(2, underlines.count())
@@ -131,6 +214,14 @@ class SpannableStringTest {
         val underline2 = underlines.last()
         assertEquals(18, s.getSpanStart(underline2))
         assertEquals(23, s.getSpanEnd(underline2))
+    }
+
+    @Test fun overlayAll_StartAfterFirst() {
+        val s = "Hello, World, I'm World".toSpannable()
+        s.overlayAll("World", false, 12) { ForegroundColorSpan(Color.BLUE) }
+
+        val spans = s.getSpans<Any>()
+        assertEquals(1, spans.size)
 
         val blue = spans.filterIsInstance<ForegroundColorSpan>().single()
         assertEquals(18, s.getSpanStart(blue))
