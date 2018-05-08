9972391d27fb3963cab848948fb1d7cd863a2c9e
Anshad <143codered@gmail.com>
2018-05-09 01:04:44 +0530

    Added number utils - Double.roundToDecimalPlaces to round the Double value to specified number of decimal values

--- ftl-tests/download-results.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-TEST_DIR=$1
-
-# Create directory for results
-mkdir "$TEST_DIR"
-
-# Pull down test results
-gsutil -m cp -r -U "`gsutil ls gs://cloud-test-android-devrel-ci | tail -1`*" "$TEST_DIR"
